5a2055891f48ed5f9e6dc495cbe2c2f1e4d41d03
cstone45 <cstone45@usf.edu>
2025-04-13 15:43:02 -0400

    Fetch and Render Tour List with useEffect and State

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import TourCard from "./TourCard";
+
+const TourList = ({tours, setTours, onRemove}) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        fetch('https://course-api.com/react-tours-project')
+        .then(response => response.json())
+        .then(data => setTours(tours));
+    }, []);
+    return (
+        <ul>
+            {tours.map(tours => (
+                <li key={tours.id}>
+                    {tour.name} - {tour.price} - {tour.image}
+                </li>
+            ))}
+        </ul>
+    );
+}
